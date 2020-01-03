6d720214a78def4cb089f7766c4bdc474baf610a
Mikka138 <mickael.almeida1308@gmail.com>
2020-01-03 17:04:53 +0100

    v0.2.1b2

---
+++ mayushii.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 5
+echo ****************************************** >> /opt/mayushii/python.log
+date >> /opt/mayushii/python.log
+python3 /opt/mayushii/bot.py >> /opt/mayushii/python.log
