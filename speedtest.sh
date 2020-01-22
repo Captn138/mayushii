119f8d2717329dc311b0a036fba4b954518deb50
Mikka138 <mickael.almeida1308@gmail.com>
2020-01-22 10:22:18 +0100

    v0.2.3

--- speedtest.sh
+++ speedtest.sh
@@ -2,4 +2,7 @@
 RECIPIENT="mickael.almeida1308@gmail.com"
 ST=$(speedtest -p no)
 echo "$ST"
-printf "Subject:SPEEDTEST\n\n$ST" | msmtp --debug --from=default -t ${RECIPIENT} > /dev/null
+content="Subject:SPEEDTEST
+
+$ST"
+echo "$content" | msmtp --debug --from=default -t ${RECIPIENT} > /dev/null
