2051aa99b84494877eee9b32972bd18bf763d33c
Mickael Almeida <malmeida@et.esiea.fr>
2021-02-05 16:37:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,4 +95,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
         print(f"Loaded {filename}")
 
-client.run(open("token.txt", "r").readline(59))
+client.run(TOKEN)
