bea2a0d39a57a32e13521b578d29563bd17818bd
Mickael Almeida <mickael.almeida@acensi.fr>
2022-04-06 16:44:56 +0200

    added else case for youy

--- cogs/regular.py
+++ cogs/regular.py
@@ -28,6 +28,8 @@ class Regular(commands.Cog):
         if __class__.last_youy is None or (days*24*60)+(hours*60)+minutes > __class__.youy_limit:
            __class__.last_youy = datetime.datetime.utcnow()
            await ctx.send("https://media.discordapp.net/attachments/622011856642637825/622012196397776896/JPEG_20181001_113150.jpg")
+        else:
+            ctx.send(f"Holà vilain garnement! Tu dois attendre {__class__.youy_limit} minutes entre chaque youy.")
 
     @commands.command()
     async def invite(self, ctx):
